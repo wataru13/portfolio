e3f09cab29cab7c2c684a24ed511138bce59caf2
wataru13 <wata13soccer@gmail.com>
2021-10-21 01:48:44 +0900

    version_up

---
+++ app/javascript/moji.js
@@ -0,0 +1,5 @@
+$("#particle").particleText({
+    text: "リモート<br><br>シェア", // 表示させたいテキスト。改行の場合は<br>追加
+    colors: ["#ffff00", "#ffff7a", "#ffff3d"], // パーティクルの色を複数指定可能
+    speed: "high" // slow, middle, high の3つから粒子が集まる速さを選択
+    });
